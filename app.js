4268817a3f290419dd7e323afd6be1e6c89ded24
Isaac Ameh <67932483+isaacamehgreg@users.noreply.github.com>
2021-05-19 12:24:11 -0700

    update

---
+++ app.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello',
+      firstName: "isaac",
+      lastName: "ameh",
+      gender:"male",
+      picture:"https://randomuser.me/api/portraits/men/10.jpg"
+
+    }
+  })
+
